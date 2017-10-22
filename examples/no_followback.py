ae47492486ae5a0aad2fd70b3bf2fc568018a1ab
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-10-23 01:19:06 +0500

    Refactoring: deleted unused import and made print compatible with py3

--- examples/no_followback.py
+++ examples/no_followback.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from twitterbot_utils import Twibot
-from tweepy import Cursor
 
 api = Twibot('strizhechenko').api
 
@@ -10,4 +9,4 @@ friends = api.friends_ids()
 no_followback = set(friends) - set(followers)
 
 for user_id in no_followback:
-    print 'https://twitter.com/{0}'.format(api.get_user(user_id).screen_name)
+    print('https://twitter.com/{0}'.format(api.get_user(user_id).screen_name))
